321826c4238cb64731976bfd00abc342733ac390
xSakix <seckarovci@gmail.com>
2018-01-31 07:33:38 +0100

    correcting simulations, that they run from day of inception and not sooner. Otherwise investor would have a lot of casch and use it at once to invest all at inception day, which could and did give him advantage over dca for succesfull etfs

--- test_etf.py
+++ test_etf.py
@@ -5,11 +5,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-sys.path.insert(0, '../rebalancer')
+sys.path.insert(0, '../etf_data')
 from etf_data_loader import load_data
 
-start_date = '1993-01-01'
-end_date = '2017-12-31'
 
 
 def rolling_mean(data, period):
@@ -26,6 +24,19 @@ def mean(value):
 
 
 def compute_one_etf(etf):
+    start_date = '1993-01-01'
+    end_date = '2017-12-31'
+
+    inception = pd.read_csv('../etf_data/etf_inc_date.csv', sep=';')
+
+    for anEtf in etf:
+        if etf in inception.ticket.values:
+            inception_date = inception[inception.ticket == anEtf].inc_date.values[0]
+            if inception_date > start_date:
+                start_date = inception_date
+
+    print(start_date)
+
     df_open, df_close, df_high, df_low, df_adj_close = load_data(etf, start_date, end_date)
     dca = bah.DCA(30, 300.)
     investor = bah.Investor(np.full(len(etf), 1.0 / len(etf)), dca)
@@ -62,10 +73,10 @@ def compute_one_etf(etf):
     for rms in rms_list:
         print(str(rms))
 
-    # ax[0].plot(np.log(df_adj_close))
-    # ax[0].plot(np.log(sim.investor.invested_history))
-    # ax[0].plot(np.log(sim.investor.history))
-    ax[0].plot(df_adj_close)
+    ax[0].plot(np.log(df_adj_close))
+    ax[0].plot(np.log(sim.investor.invested_history))
+    ax[0].plot(np.log(sim.investor.history))
+    # ax[0].plot(sim.investor.history)
     ax[1].plot(sim.investor.ror_history)
     ax[0].legend(['nav', 'invested', 'value'])
     ax[1].legend(['RoR'])
@@ -73,4 +84,4 @@ def compute_one_etf(etf):
     plt.show()
 
 
-compute_one_etf(['JSML'])
+compute_one_etf(['PAK'])
--- test_one_etf.py
+++ test_one_etf.py
@@ -5,28 +5,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-sys.path.insert(0, '../rebalancer')
+sys.path.insert(0, '../etf_data')
 from etf_data_loader import load_data
 
 with open('../rebalancer/etfs.txt', 'r') as fd:
     etfs = list(fd.read().splitlines())
 
 etfs = list(set(etfs))
-
-start_date = '1993-01-01'
-end_date = '2017-12-31'
-
-results = 'results/result.csv'
-if os.path.isfile(results):
-    os.remove(results)
-
-with open(results, 'a+') as fd:
-    headers = 'ticket;invested;value;ror;mean;std'
-    for i in range(1, 11):
-        headers += ';ma ror '+str(i)+' years'
-
-
-    fd.write(headers+'\n')
 
 
 def rolling_mean(data, period):
@@ -42,10 +27,17 @@ def mean(value):
     return value
 
 
-def compute_one_etf():
-    global fd
-    print(etf)
+def compute_one_etf(etf, inception, results):
     result = etf
+
+    start_date = '1993-01-01'
+    end_date = '2017-12-31'
+
+    if etf in inception.ticket.values:
+        inception_date = inception[inception.ticket == etf].inc_date.values[0]
+        if inception_date > start_date:
+            start_date = inception_date
+
     df_open, df_close, df_high, df_low, df_adj_close = load_data([etf], start_date, end_date)
     dca = bah.DCA(30, 300.)
     investor = bah.Investor([1.0], dca)
@@ -76,32 +68,31 @@ def compute_one_etf():
     result += ';' + str(m)
     result += ';' + str(std)
     for rms in rms_list:
-        result += ';'+str(rms)
+        result += ';' + str(rms)
     result += '\n'
 
     with open(results, 'a+') as fd:
         fd.write(result)
 
 
+results = 'results/result.csv'
+if os.path.isfile(results):
+    os.remove(results)
+
+with open(results, 'a+') as fd:
+    headers = 'ticket;invested;value;ror;mean;std'
+    for i in range(1, 11):
+        headers += ';ma ror ' + str(i) + ' years'
+
+    fd.write(headers + '\n')
+
+inception = pd.read_csv('../etf_data/etf_inc_date.csv', sep=';')
+
+count = 1
 for etf in etfs:
     try:
-        compute_one_etf()
+        print(str(count) + '/' + str(len(etfs)))
+        count += 1
+        compute_one_etf(etf, inception, results)
     except:
         print(etf + ' not processed')
-
-    # _,ax = plt.subplots(3,1)
-    # ax[0].plot(np.log(df_adj_close))
-    # ax[0].plot(np.log(sim.investor.invested_history))
-    # ax[0].plot(np.log(sim.investor.history))
-    # ax[1].plot(sim.investor.ror_history)
-    # ax[2].plot(rm1)
-    # ax[2].plot(rm2)
-    # ax[2].plot(rm3)
-    # ax[2].plot(rm5)
-    # ax[2].plot(rm7)
-    # ax[2].plot(rm10)
-    # ax[0].legend(['nav', 'invested', 'value'])
-    # ax[1].legend(['RoR'])
-    # ax[2].legend(['MA RoR 1 year','MA RoR 2 years','MA RoR 3 years','MA RoR 5 years','MA RoR 7 years','MA RoR 10 years'])
-
-    # plt.show()
