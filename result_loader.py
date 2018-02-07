1ba2d886b651778ea333bcad800ca8f02f2cbcf2
xSakix <seckarovci@gmail.com>
2018-02-07 16:16:40 +0100

    loading ranked data + using ranked etf in testing of computed mean of returuns vs observed from chaos simulations

---
+++ result_loader.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import os
+
+
+def load_ranked(prefix=''):
+    file = 'results/' + prefix + 'ranked_result.csv'
+    if not os.path.isfile(file):
+        file = '../buy_hold_simulation/' + file
+
+    data = pd.read_csv(file)
+    return data.sort_values(by=['rank'], ascending=False)
